ed214681bdf00c63c275abde67be82e5d5912a8d
Golpeabuelas <chavez.garcia.julian2@gmail.com>
2025-04-09 12:10:21 -0600

    Avanzando con los tabs en react native

---
+++ components/icons.jsx
@@ -0,0 +1,13 @@
+import { FontAwesome6 } from "@expo/vector-icons"
+
+export default function AddUserIcon() {
+    return (
+        <FontAwesome6 name="user-plus" size={24} color="black" />
+    )
+}
+
+export default function UserIcon() {
+    return(
+        <FontAwesome6 name="user-large" size={24} color="black" />
+    )
+}
--- components/tabs.jsx
+++ components/tabs.jsx
@@ -1,14 +1,20 @@
 import Logo from "../assets/images/logo.png"
 import { FontAwesome5 } from "@expo/vector-icons";
-import { Image, View } from "react-native";
+import { Dimensions, Image, Text, View } from "react-native";
+
+const height = Dimensions.get('window').height
 
 export default function Tabs() {
     return(
-        <View className="w-full h-[11%] bg-[#FFBD59] flex-row justify-between items-center px-[10%]">
-            <FontAwesome5 name="user-alt" size={48} color="#D9D1C8" className="z-1"/>
-            <Image source={Logo} className="w-24 h-24 mb-3 z-1"/>
-            <FontAwesome5 name="user-plus" size={48} color="#D9D1C8" className="z-1"/>
-            <View className="w-26 h-26 rounded-full bg-[#D9D9D9] z-0"></View>
+        <View style={{ paddingInline: height*0.043}} className="w-full h-[11%] bg-[#FFBD59] flex-row absolute bottom-0 rounded-b-[25] border-solid border-[#000] border-b-[25]">
+            <View className="w-[90%] flex-row">
+                <View></View>
+                <View style={{ width: height*0.075, height: height*0.075, marginBottom: height*0.0187, marginTop: height*0.0176}} className="border-[#000] border-solid border"></View>
+                <View style={{ width: height*0.075, height: height*0.075, marginBottom: height*0.0187, marginTop: height*0.0176}} className="border-[#000] border-solid border"></View>
+                <View style={{ width: height*0.075, height: height*0.075, marginBottom: height*0.0187, marginTop: height*0.0176}} className="border-[#000] border-solid border"></View>
+            </View>
+            
+            <View style={{ width: height*0.0927, height: height*0.0927, borderWidth: height*0.0064}} className="bg-[#D9D9D9] border-[#E9E9E9] border-solid rounded-full relative top-[-50%] z-1"></View>
         </View>
     )
 }
